55d83f23b7f1f7479db7be7de6cabdb00d4c118f
CristiHabliuc <cristi.habliuc@gmail.com>
2019-03-30 17:40:13 +0200

    Fixes #53: Replaced the forms UI with a collection view to display the forms fetched from the API instead of the hard coded 3 forms; Updated the form persistance to a cleaner structure (it may still be better to store it with an advanced solution, but it works for now);

--- MonitorizareVot/LocalFormsPersistor.swift
+++ MonitorizareVot/LocalFormsPersistor.swift
@@ -2,28 +2,77 @@
 
 import Foundation
 
+
 class LocalFormsPersistor: FormsPersistor {
+
+    fileprivate enum UserDefaultsKeys: String {
+        case forms = "forms"
+        case formPrefix = "form:"
+    }
     
+    // MARK: - Public
+    
+    /// Saves the form summary (id and version), along with the form details
     func save(version: Int, name: String, informations: [[String : AnyObject]]) {
-        UserDefaults.standard.set(version, forKey: name + "version")
-        let data = NSKeyedArchiver.archivedData(withRootObject: informations)
-        UserDefaults.standard.set(data, forKey: name + "informations")
+        saveFormSummary(withId: name, version: version)
+        saveFormSections(withId: name, sections: informations)
+    }
+    
+    /// Returns the list of all form ids
+    func getAllForms() -> [String] {
+        let forms = getAllFormSummaries()
+        return Array(forms.keys).sorted()
+    }
+    
+    /// Fetches the version of the specified form or 0 if not found
+    func getVersion(forForm id: String) -> Int {
+        let forms = getAllFormSummaries()
+        return forms[id] ?? 0
     }
 
-    func getVersion(forForm: String) -> Int {
-        if let version = UserDefaults.standard.value(forKey: forForm + "version") as? Int {
-            return version
-        }
-        return 0
+    /// Fetches the full form from storage
+    func getInformations(forForm id: String) -> [[String :AnyObject]]? {
+        let key = UserDefaultsKeys.formPrefix.rawValue + id
+        
+        guard let data = UserDefaults.standard.value(forKey: key) as? Data else { return nil }
+        guard let form = NSKeyedUnarchiver.unarchiveObject(with: data) as? [[String :AnyObject]] else { return nil }
+        
+        return form
     }
-
-    func getInformations(forForm: String) -> [[String :AnyObject]]? {
-        if let data = UserDefaults.standard.value(forKey: forForm + "informations") as? Data {
-            if let informations = NSKeyedUnarchiver.unarchiveObject(with: data) as? [[String: AnyObject]] {
-                return informations
+    
+    // MARK: - Internals
+    
+    /// Saves the form summary as a hash ([id: version])
+    fileprivate func saveFormSummary(withId id: String, version: Int) {
+        let key = UserDefaultsKeys.forms.rawValue
+        var forms: [String: Int] = [:]
+        if let data = UserDefaults.standard.value(forKey: key) as? Data {
+            if let object = NSKeyedUnarchiver.unarchiveObject(with: data) as? [String: Int] {
+                forms = object
             }
         }
-        return nil
+        
+        forms[id] = version
+        
+        let data = NSKeyedArchiver.archivedData(withRootObject: forms)
+        UserDefaults.standard.set(data, forKey: key)
+        UserDefaults.standard.synchronize()
+    }
+    
+    fileprivate func saveFormSections(withId id: String, sections: [[String: AnyObject]]) {
+        let key = UserDefaultsKeys.formPrefix.rawValue + id
+        
+        let data = NSKeyedArchiver.archivedData(withRootObject: sections)
+        UserDefaults.standard.set(data, forKey: key)
+        UserDefaults.standard.synchronize()
+    }
+    
+    fileprivate func getAllFormSummaries() -> [String: Int] {
+        guard let data = UserDefaults.standard.value(forKey: UserDefaultsKeys.forms.rawValue) as? Data else { return [:] }
+        
+        guard let forms = NSKeyedUnarchiver.unarchiveObject(with: data) as? [String: Int] else { return [:] }
+        
+        return forms
     }
     
 }
